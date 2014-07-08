2c1a38102ec416461a221e027f34bafd7d7ed8e5
BYWallace <bwallace90@gmail.com>
2014-07-08 11:36:00 -0400

    added fixture to model

--- js/models/todo.js
+++ js/models/todo.js
@@ -2,3 +2,21 @@ Todos.Todo = DS.Model.extend({
   title: DS.attr("string"),
   isCompleted: DS.attr("boolean")
 });
+
+Todos.Todo.FIXTURES = [
+  {
+    id: 1,
+    title: "Learn Ember.js",
+    isCompleted: true
+  },
+  {
+    id: 2,
+    title: "Build a portfolio site",
+    isCompleted: false
+  },
+  {
+    id: 3,
+    title: "Grow a handlebar mustache",
+    isCompleted: false
+  }
+];
